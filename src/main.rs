240d304ec3b144a5becc7e3abd50c9ce15f17ddb
Ruslan Fadeev <kinrany@yandex.com>
2023-01-02 00:57:05 +0300

    configure websockets

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::{io, net::SocketAddr};
 
-use axum::{http::StatusCode, response::IntoResponse, routing::get_service, Router, Server};
+use axum::{
+    extract::{ws::WebSocket, WebSocketUpgrade},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, get_service},
+    Router, Server,
+};
 use tower_http::{
     services::{ServeDir, ServeFile},
     trace::TraceLayer,
 };
-use tracing::info;
+use tracing::{error, info, warn};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -27,8 +33,38 @@ async fn tracing_setup() {
 fn router() -> Router {
     let dir = ServeDir::new("static").not_found_service(ServeFile::new("static/not_found.txt"));
     Router::new()
+        .route("/ws", get(ws_handler))
         .nest_service("/", get_service(dir).handle_error(handle_error))
         .layer(TraceLayer::new_for_http())
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        match msg {
+            Ok(msg) => {
+                let message = msg.into_text().unwrap();
+                let response = if message == "ping" {
+                    "pong"
+                } else {
+                    info!(%message, "incoming websocket message");
+                    "what?"
+                };
+                if let Err(error) = socket.send(response.into()).await {
+                    error!(%error, "outgoing websocket error");
+                }
+            }
+            Err(error) => {
+                warn!(%error, "incoming websocket error");
+                if let Err(error) = socket.send(error.to_string().into()).await {
+                    error!(%error, "outgoing websocket error");
+                }
+            }
+        }
+    }
 }
 
 async fn serve() {
--- static/index.js
+++ static/index.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+let socket = new WebSocket("ws://127.0.0.1:3000/ws");
+
+socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    console.log("Sending to server");
+    socket.send("My name is John");
+    socket.send("ping");
+};
+
+socket.onmessage = function (event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log('[close] Connection died');
+    }
+};
+
+socket.onerror = function (error) {
+    console.error(`[error]`);
+};
