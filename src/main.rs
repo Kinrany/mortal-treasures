24c10173c0fd2d55b21e63051ecd275fef05a357
Ruslan Fadeev <kinrany@yandex.com>
2023-01-02 05:47:22 +0300

    don't log tower_http

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn tracing_setup() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info,mortal_treasures=debug,tower_http=debug".into()),
+                .unwrap_or_else(|_| "info,mortal_treasures=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
