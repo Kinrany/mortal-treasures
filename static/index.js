a175fea96f820ab1fcb49ff0665c3b52919fd932
Ruslan Fadeev <kinrany@yandex.com>
2023-01-08 13:58:57 +0300

    vue frontend

--- static/index.js
+++ static/index.js
@@ -1,3 +1,4 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.global.js';
 import init, { apply_event } from './wasm/mortal_treasures_world.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,57 +10,58 @@ async function main() {
     await init();
     console.log('[load] WASM loaded.');
 
-    const counter_el = document.getElementById('counter');
-    const increment_el = document.getElementById('increment');
-    const decrement_el = document.getElementById('decrement');
-    const text_el = document.getElementById('text');
-    const game_over_el = document.getElementById('game_over');
+    app().mount('#app');
+}
 
-    let world = { count: 5, text: "hello???" };
+function app() {
+    createApp({
+        data() {
+            return {
+                world: {
+                    count: 5,
+                    text: 'hello???',
+                },
+                socket: null,
+            }
+        },
+        methods: {
+            increment() {
+                socket.send(JSON.stringify({ kind: 'Increment' }));
+            },
+            decrement() {
+                socket.send(JSON.stringify({ kind: 'Decrement' }));
+            },
+            set_text(e) {
+                socket.send(JSON.stringify({ kind: 'Text', s: e.value }));
+            },
+            game_over() {
+                socket.send(JSON.stringify({ kind: 'GameOver' }));
+            }
+        },
+        mounted() {
+            const socket = new WebSocket("ws://127.0.0.1:3000/ws");
 
-    const display = () => {
-        counter_el.innerText = world.count.toString();
-        text_el.value = world.text;
-        console.log(`[state] world: ${JSON.stringify(world)}`);
-    };
-    const handle_event = (e) => {
-        world = apply_event(world, e);
-        display();
-    }
+            socket.onmessage = (event) => {
+                let m = event.data;
+                console.log(`[message] Data received from server: ${m}`);
+                let v = JSON.parse(m);
+                this.world = apply_event(this.world, v);
+            };
 
-    const socket = new WebSocket("ws://127.0.0.1:3000/ws");
 
-    socket.onopen = function (e) {
-        console.log("[open] Connection established");
+            socket.onclose = (event) => {
+                if (event.wasClean) {
+                    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+                } else {
+                    // e.g. server process killed or network down
+                    // event.code is usually 1006 in this case
+                    console.log('[close] Connection died');
+                }
+            };
 
-        increment_el.onclick = () => socket.send(JSON.stringify({ kind: 'Increment' }));
-        decrement_el.onclick = () => socket.send(JSON.stringify({ kind: 'Decrement' }));
-        text_el.onchange = function () {
-            let s = this.value;
-            display();
-            socket.send(JSON.stringify({ kind: 'Text', s }));
-        };
-        game_over_el.onclick = () => socket.send(JSON.stringify({ kind: 'GameOver' }));
-    };
+            socket.onerror = () => console.error(`[error]`);
 
-    socket.onmessage = function (event) {
-        let m = event.data;
-        console.log(`[message] Data received from server: ${m}`);
-        let v = JSON.parse(m);
-        handle_event(v);
-    };
-
-    socket.onclose = function (event) {
-        if (event.wasClean) {
-            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-        } else {
-            // e.g. server process killed or network down
-            // event.code is usually 1006 in this case
-            console.log('[close] Connection died');
+            this.socket = socket;
         }
-    };
-
-    socket.onerror = function (error) {
-        console.error(`[error]`);
-    };
+    })
 }
